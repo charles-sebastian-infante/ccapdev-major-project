a614ef7211009c19bad5fa252af15791ad247047
Respille <christiangendrano@gmail.com>
2026-02-24 22:09:51 +0800

    feat: importing chart sample data to database

---
+++ src/database/models/Chart.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const ChartSchema = new mongoose.Schema({
+    _id: ObjectId,
+    songName: String,
+    songAuthor: String,
+    charterName: String, // replace with charterId later
+    numericRating: Number,
+    difficultyLevel: {
+        type: String,
+        enum: ['Expert', 'Master', 'Re:Master']
+    },
+    imagePath: String
+});
+
+const Chart = mongoose.model('Chart', ChartSchema);
+
+module.exports = Chart;
--- src/index.js
+++ src/index.js
@@ -7,6 +7,31 @@ app.use(express.static(path.join(__dirname, "public")));
 // add these if necessary
 // app.use(express.urlencoded({extended: false}));
 
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/offbeatDB");
+
+const Chart = require("./database/models/Chart");
+
+/* handling command line arguments (we can use them to manipulate
+   the database, at least for now) */
+const args = process.argv;
+if (args.length === 3) { // if there's one additional argument
+    const arg = args[2];
+    if (arg === "clear-db") {
+        (async () => {
+            await Chart.deleteMany({});
+            console.log("Database has been cleared");
+        })();
+    } else if (arg === "insert-sample-data") {
+        // the sample data is stored in JSON files
+        const chartSampleData = require("./database/sample_data/sample_charts.json");
+        (async () => {
+            await Chart.insertMany(chartSampleData);
+            console.log("Sample data has been added");
+        })();
+    }
+}
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "pages", "index.html"))
 });
