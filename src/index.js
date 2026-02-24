b3a67310bc4cc4576b436a9e0ec8c40ce135dad5
Respille <christiangendrano@gmail.com>
2026-02-24 15:28:42 +0800

    refactor: added back the folder for pages

--- src/index.js
+++ src/index.js
@@ -7,6 +7,28 @@ app.use(express.static(path.join(__dirname, "public")));
 // add these if necessary
 // app.use(express.urlencoded({extended: false}));
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "pages", "index.html"))
+});
+
+// add better method later
+const pages = [
+    "edit_profile",
+    "signup",
+    "view_profile",
+    "emperror_exp",
+    "emperror_mas",
+    "sample_exp",
+    "sample_mas",
+    "sample_remas",
+];
+
+pages.forEach(page => {
+    app.get("/" + page, (req, res) => {
+        res.sendFile(path.join(__dirname, "pages", page + ".html"));
+    });
+});
+
 app.listen(3000, () => {
     console.log("Server running at port 3000");
 })
